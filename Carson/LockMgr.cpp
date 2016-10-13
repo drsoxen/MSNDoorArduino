b776fb8f6b6228b3bd96090994a0d65dbd2e373c
Drsoxen <drsoxen@gmail.com>
2016-10-13 19:33:22 -0400

    added active high

--- Carson/LockMgr.cpp
+++ Carson/LockMgr.cpp
@@ -1,23 +1,34 @@
 #include "LockMgr.h"
 
 int g_lockPin;
+bool activeHigh = false;
 LockMgr::LockMgr(int lockPin, TimerMgr* timerMgr)
 {
   m_lockPin = lockPin;
   g_lockPin = m_lockPin;
   m_timer = timerMgr;
   pinMode(m_lockPin, OUTPUT);
-  digitalWrite(m_lockPin, LOW);
+  
+  if(activeHigh)
+    digitalWrite(m_lockPin, LOW);
+  else
+    digitalWrite(m_lockPin, HIGH);
 }
 
 void lock()
 {
-  digitalWrite(g_lockPin, HIGH);
+  if(activeHigh)
+    digitalWrite(g_lockPin, HIGH);
+  else
+    digitalWrite(g_lockPin, LOW);
 }
 
 void unlock()
 {
-  digitalWrite(g_lockPin, LOW);
+    if(activeHigh)
+    digitalWrite(g_lockPin, LOW);
+  else
+    digitalWrite(g_lockPin, HIGH);
 }
 
 void LockMgr::changeLockState(LockStates state)
